2d1a342657b653dd535ef952a42a826d85ad4f02
Yash Jain <yash0307jain@gmail.com>
2019-06-08 21:13:57 +0530

    weather and location api implementation on running

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
-// const request = require('request');
-const request = require('request-promise');
+const request = require("request");
+// const request = require('request-promise');
 
-const url = 'https://api.darksky.net/forecast/dd2cd4dd1b7014b4dc0135d4890fc31a/26.2183,78.1828?units=si';
+/////////////////////////////////// DARKSKY API ///////////////////////////////////
+const darkskyUrl =
+  "https://api.darksky.net/forecast/dd2cd4dd1b7014b4dc0135d4890fc31a/26.2183,78.1828?units=si";
 
-/*
-if you are giving arguments inside the reqest function directly
-then it is necessary to first call the body then you can access the json objects properties
-*/
-request({ url: url, json: true }, (error, response) => {
-  console.log('//////////////////////////// 1st FUNCTION ////////////////////////////')
-  const res = response.body;
-  console.log(res.currently);
-  console.log('There is ' + res.currently.temperature + ' degrees out. There is a ' + res.currently.precipProbability + '% chance of rain.');
+request({ url: darkskyUrl, json: true }, (err, res) => {
+  if (err) {
+    console.log("Unable to connect to weather service!");
+  } else {
+    if (res.body.error) {
+      console.log(res.body.error);
+    } else {
+      console.log(
+        res.body.daily.data[0].summary +
+          " There is " +
+          res.body.currently.temperature +
+          " degrees out. There is a " +
+          res.body.currently.precipProbability +
+          "% chance of rain."
+      );
+    }
+  }
 });
 
+/////////////////////////////////// GE0CODING API ///////////////////////////////////
+const geocodeUrl =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieWFzaGphaW4wMzA3IiwiYSI6ImNqd245Z3M5ZDBmem80OG96a21icm81b2wifQ.1Dl3xZCvCKE_FYR4v5epkA";
 
-/*
-if you are using promise and in then function you are using response
-then this is directly coming from body and there is no need to fetch this from body
-therefore you can directly call the json object properties
-*/
-request({ url: url, json: true })
-  .then((res) => {
-    console.log('//////////////////////////// 2nd FUNCTION ////////////////////////////')
-    console.log(res.currently);
-    console.log('There is ' + res.currently.temperature + ' degrees out. There is a ' + res.currently.precipProbability + '% chance of rain.');
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+request({ url: geocodeUrl, json: true }, (err, res) => {
+  if (err) {
+    console.log("Unable to connect to location servive!");
+  } else {
+    if (res.body.message) {
+      console.log(res.body.message);
+    } else {
+      const latitude = res.body.features[0].center[0];
+      const longitude = res.body.features[0].center[1];
+      console.log(latitude, longitude);
+    }
+  }
+});

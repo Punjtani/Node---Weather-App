b0fd8cbcdfc593e335b06f78ac785df269bfe4b8
Yash Jain <yash0307jain@gmail.com>
2019-06-14 10:47:46 +0530

    frontend complete and process of integrating it with backend is going on

---
+++ src/app.js
@@ -0,0 +1,88 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+
+const forecast = require('./utils/forecast');
+const location = require('./utils/geocode');
+
+// Define path for express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath); // with this you dont have to stick with folder name views
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Yash Jain"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Yash Jain"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text",
+    title: "Help",
+    name: "Yash Jain"
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if(!req.query.address) {
+    return res.send({
+      address: "You must provide an address!"
+    });
+  };
+
+  location(req.query.address, (err, { latitude, longitude, location }) => {
+    if (err) {
+      return res.send({ err });
+    }
+
+    forecast(latitude, longitude, (err, forecastData) => {
+      if (err) {
+        return res.send({ err });;
+      }
+
+      res.send({
+        forecast: forecastData,
+        location: location,
+        address: req.query.address
+      });
+    })
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Yash Jain",
+    errorMsg: "Help article not found."
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Yash Jain",
+    errorMsg: "Page not found."
+  });
+});
+
+const port = 3000;
+app.listen(port, () => console.log("Server is listening on port", port));
---
+++ src/utils/forecast.js
@@ -0,0 +1,30 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "https://api.darksky.net/forecast/dd2cd4dd1b7014b4dc0135d4890fc31a/" +
+    longitude +
+    "," +
+    latitude +
+    "?units=si";
+
+  request({ url, json: true }, (err, { body }) => {
+    if (err) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          " There is " +
+          body.currently.temperature +
+          " degrees out. There is a " +
+          body.currently.precipProbability +
+          "% chance of rain."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
